aaa4949c482b67fbee0e027148bc7f0942e0e95c
Kapil Singh Negi <124447041+kapilsinghnegi@users.noreply.github.com>
2024-09-06 12:32:31 +0530

    Add buttons for different functionalities
    - Added clear grid button which when clicked clears the grid container cells to white color
    - Added random color button which changes grid's cell's color to random rgb values
    - Added black color button that changes grid's cell's color to black color
    - Added eraser button that changes grid's cell's color to white color

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 'use strict';
 
-const size = document.getElementById('grid-size-button');
 const gridContainer = document.getElementsByClassName('grid-container');
+const gridSize = document.getElementById('grid-size-button');
+const clearButton = document.getElementById('clear-button');
+const randomColorButton = document.getElementById('random-color-button');
+const eraserButton = document.getElementById('eraser-button');
+const blackColorButton = document.getElementById('black-color-button');
 
 function createGrid(size) {
   for (let i = 0; i < size; i++) {
@@ -13,6 +17,7 @@ function createGrid(size) {
       cell.classList.add('grid-cell');
       cell.style.width = `${500 / size}px`;
       cell.style.height = `${500 / size}px`;
+      cell.style.backgroundColor = 'white';
       row.appendChild(cell);
       cell.addEventListener('mouseover', () => {
         cell.style.backgroundColor = 'black';
@@ -22,7 +27,7 @@ function createGrid(size) {
 }
 createGrid(16);
 
-size.addEventListener('click', () => {
+gridSize.addEventListener('click', () => {
   const newSize = prompt('Enter new grid size: ');
   if (newSize > 100 || newSize < 1) {
     alert('Please enter a number between 1 and 100');
@@ -32,4 +37,42 @@ size.addEventListener('click', () => {
     }
     createGrid(newSize);
   }
+});
+
+clearButton.addEventListener('click', () => {
+  const cells = document.querySelectorAll('.grid-cell');
+  cells.forEach(cell => {
+    cell.style.backgroundColor = 'white';
+  });
+});
+
+randomColorButton.addEventListener('click', () => {
+  const cells = document.querySelectorAll('.grid-cell');
+  cells.forEach(cell => {
+    cell.addEventListener('mouseover', () => {
+      cell.style.backgroundColor = `rgb(${Math.floor(
+        Math.random() * 256
+      )}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
+        Math.random() * 256
+      )})`;
+    });
+  });
+});
+
+eraserButton.addEventListener('click', () => {
+  const cells = document.querySelectorAll('.grid-cell');
+  cells.forEach(cell => {
+    cell.addEventListener('mouseover', () => {
+      cell.style.backgroundColor = 'white';
+    });
+  });
+});
+
+blackColorButton.addEventListener('click', () => {
+  const cells = document.querySelectorAll('.grid-cell');
+  cells.forEach(cell => {
+    cell.addEventListener('mouseover', () => {
+      cell.style.backgroundColor = 'black';
+    });
+  });
 });
